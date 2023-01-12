02d386606b4f960e0e72b6081cf18a17913f20e6
Chihwei <chihwei.cheng@tradevan.com.tw>
2023-01-12 17:25:46 +0800

    調整有趣的文字敘述

--- FolderHelper/Program.cs
+++ FolderHelper/Program.cs
@@ -26,28 +26,26 @@ namespace FolderHelper
             {
                 Console.WriteLine("羅伊德：題目是：讀取指定路徑下所有檔名，並匯出FileList.xlsx");
                 Console.WriteLine("阿尼亞：T_T");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
                 helper.GetAllFilesNameExportExcel(sourcePath, targetPath);
-                Console.WriteLine("...");
-                Console.WriteLine("...");
-                Console.WriteLine("...");
-                Console.WriteLine("羅伊德：好厲害，完成了!!");
-                Console.WriteLine("阿尼亞：V_V");
             }
             else if (model == "2")
             {
                 Console.WriteLine("羅伊德：題目是：讀取FileList.xlsx並依內容將檔案重新命名");
                 Console.WriteLine("阿尼亞：Q_Q");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
+                Console.WriteLine("...");
                 helper.RenameFileFromExcel(targetPath);
-                Console.WriteLine("...");
-                Console.WriteLine("...");
-                Console.WriteLine("...");
-                Console.WriteLine("羅伊德：好厲害，完成了!!");
-                Console.WriteLine("阿尼亞：V_V");
+                
             }
             else
             {
                 Console.WriteLine("羅伊德：恩!?今天沒有作業");
             }
+
             Console.WriteLine("約  兒：這樣的話，我來做晚餐吧!!");
             Console.WriteLine("羅伊德：!!");
             Console.WriteLine("阿尼亞：!!");
@@ -70,11 +68,14 @@ namespace FolderHelper
                 setDataTableSchema();
                 dirSearch(folderPath);
                 Excel.ExportExcelFromDataTable(baseDT, excelFile);
+
+                Console.WriteLine("羅伊德：好厲害，完成了!!");
+                Console.WriteLine("阿尼亞：V_V");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("");
-                Console.WriteLine("阿尼亞不會寫程式，把這個交給父親");
+                Console.WriteLine("阿尼亞不會寫程式，把這個問題交給父親");
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("");
             }
@@ -86,11 +87,14 @@ namespace FolderHelper
             {
                 baseDT = Excel.LoadExcelAsDataTable(excelPath);
                 renameFile(baseDT);
+
+                Console.WriteLine("羅伊德：好厲害，完成了!!");
+                Console.WriteLine("阿尼亞：V_V");
             }
             catch (Exception ex)
             {
                 Console.WriteLine("");
-                Console.WriteLine("阿尼亞不會寫程式，把這個交給父親");
+                Console.WriteLine("阿尼亞不會寫程式，把這個問題交給父親");
                 Console.WriteLine(ex.Message);
                 Console.WriteLine("");
             }
